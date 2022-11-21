b9e4d1388d0e7ba33930b858ab1f2987106787c0
VoyVivika <52683064+VoyVivika@users.noreply.github.com>
2022-11-20 23:05:53 -0500

    Whoops I left the CCK in there

--- Assets/ABI.CCK/Components/CVRBlitter.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class CVRBlitter : MonoBehaviour
-{
-    [SerializeField] RenderTexture originTexture = null;
-    [SerializeField] RenderTexture destinationTexture = null;
-    [SerializeField] Material blitMaterial = null;
-    public bool clearEveryFrame;
-}
